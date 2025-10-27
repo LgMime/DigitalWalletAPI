75297bcabae6244921cdd1ff3cbbfb3df7c6f40e
LGKaleka <Bizubik228@gmail.com>
2025-10-28 00:16:05 +0200

    add yml

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,12 +1,9 @@
-// Це файл: WebApplication1/Program.cs
 
-using WebApplication1; // Додаємо, щоб проєкт "бачив" наш MathService
+using WebApplication1; 
 
 var builder = WebApplication.CreateBuilder(args);
 
-// --- 1. Додаємо сервіси ---
 
-// Додаємо наш MathService, щоб API могло ним користуватись
 builder.Services.AddSingleton<MathService>();
 
 
@@ -20,9 +17,6 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-
-// --- 3. Створюємо наші API-ендпоінти (для Завдання 3) ---
-
 // Ендпоінт: GET /users
 app.MapGet("/users", () =>
 {
@@ -35,11 +29,7 @@ app.MapPost("/users", (User userRequest) =>
     return Results.Created($"/users/{userRequest.Name}", userRequest);
 });
 
-// --- 4. Запускаємо додаток ---
 app.Run();
-//1
-//2
-//3
 
 // Допоміжний клас, який потрібен для POST /users
 public class User
